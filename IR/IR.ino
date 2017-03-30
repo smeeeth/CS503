722a8787eb776f194236fa8567cdc72c4a311957
Michael Limbird <m-limbird@onu.edu>
2017-03-30 13:38:26 -0400

    added second encoder code to the IR.ino

--- IR/IR.ino
+++ IR/IR.ino
@@ -11,81 +11,66 @@
 
 #define encoder0PinA  2
 #define encoder0PinB  4
+#define encoder1PinA  3
+#define encoder1PinB  5
 
 volatile unsigned int encoder0Pos = 0;
-void doEncoder();
+volatile unsigned int encoder1Pos = 0;
+
+void doEncoder0();
+void doEncoder1();
+
+
 void setup() { 
 
-
+  //setup encoder 0
   pinMode(encoder0PinA, INPUT); 
   digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
   pinMode(encoder0PinB, INPUT); 
   digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
+  attachInterrupt(0, doEncoder0, CHANGE);  // encoder pin on interrupt 0 - pin 2
 
-  attachInterrupt(0, doEncoder, CHANGE);  // encoder pin on interrupt 0 - pin 2
+  //setup encoder 1
+  
+  pinMode(encoder1PinA, INPUT); 
+  digitalWrite(encoder1PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder1PinB, INPUT); 
+  digitalWrite(encoder1PinB, HIGH);       // turn on pull-up resistor
+  attachInterrupt(1, doEncoder1, CHANGE);  // encoder pin on interrupt 0 - pin 2
+
+  
   Serial.begin (9600);
   Serial.println("start");                // a personal quirk
 
 } 
 
 int counter = 0;
-bool last_B_read = LOW;
+bool last_B0_read = LOW;
+bool last_B1_read = LOW;
 
 void loop(){
 // do some stuff here - the joy of interrupts is that they take care of themselves
-
-  counter++;
-  
 }
 
 
 /* See this expanded function to get a better understanding of the
  * meanings of the four possible (pinA, pinB) value pairs:
  */
-void doEncoder(){
+void doEncoder0(){
     bool current_B = digitalRead(encoder0PinB);
-    if (current_B != last_B_read) {
+    if (current_B != last_B0_read) {
       encoder0Pos = encoder0Pos + 1;
-      last_B_read = current_B;
+      last_B0_read = current_B;
     }
     Serial.println (encoder0Pos, DEC);
 }
-/*void doEncoder() {
-  /* If pinA and pinB are both high or both low, it is spinning
-   * forward. If they're different, it's going backward.
-   *
-   * For more information on speeding up this process, see
-   * [Reference/PortManipulation], specifically the PIND register.
-   */
- /* if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
-    encoder0Pos++;
-  } else {
-    encoder0Pos--;
-  }*/
 
-  //Serial.println (encoder0Pos, DEC);
-//}
-
-/* See this expanded function to get a better understanding of the
- * meanings of the four possible (pinA, pinB) value pairs:
- */
-/*void doEncoder_Expanded(){
-  if (digitalRead(encoder0PinA) == HIGH) {   // found a low-to-high on channel A
-     // check channel B to see which way
-                                             // encoder is turning
-      encoder0Pos = encoder0Pos + 1;         // CCW
-    } 
-    else {
-      encoder0Pos = encoder0Pos - 1;         // CW
+void doEncoder1(){
+    bool current_B = digitalRead(encoder1PinB);
+    if (current_B != last_B0_read) {
+      encoder1Pos = encoder1Pos + 1;
+      last_B1_read = current_B;
     }
- 
-  Serial.println (encoder0Pos, DEC);          // debug - remember to comment out
-                                              // before final program run
-  // you don't want serial slowing down your program if not needed
+    Serial.println (encoder1Pos, DEC);
 }
-/*  to read the other two transitions - just use another attachInterrupt()
-in the setup and duplicate the doEncoder function into say, 
-doEncoderA and doEncoderB. 
-You also need to move the other encoder wire over to pin 3 (interrupt 1). 
-*/
 
